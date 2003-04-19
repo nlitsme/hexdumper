0bb9438fd06f4f33eb3d95af01a9d215d101a3ed
itsme <itsme@xs4all.nl>
2003-04-19 02:48:47 +0000

    made option processing more like the other tools

--- itsutils/src/dump.cpp
+++ itsutils/src/dump.cpp
@@ -82,15 +82,18 @@ int main(int argc, char **argv)
     DWORD dwBaseOffset=0;
     char *szFilename=NULL;
 
+#define HANDLEULOPTION(var, type) if (argv[i][2]) { var= (type)strtoul(argv[i]+2, 0, 0); } else { if (i+1<argc) var= (type)strtoul(argv[++i], 0, 0); }
+#define HANDLESTROPTION(var) if (argv[i][2]) { var= argv[i]+2; } else { if (i+1<argc) var= argv[++i]; }
+
     for (int i=1 ; i<argc ; i++)
     {
         if (argv[i][0]=='-')
             switch (argv[i][1])
             {
-                case 'b': dwBaseOffset= strtoul(argv[i]+2, 0, 0); break;
-                case 'o': dwOffset= strtoul(argv[i]+2, 0, 0); break;
-                case 'l': nLength= strtol(argv[i]+2, 0, 0); break;
-                case 'f': szFilename= argv[i]+2; break;
+                case 'b': HANDLEULOPTION(dwBaseOffset, DWORD); break;
+                case 'o': HANDLEULOPTION(dwOffset, DWORD); break;
+                case 'l': HANDLEULOPTION(nLength, DWORD); break;
+                case 'f': HANDLESTROPTION(szFilename); break;
             }
     }
 
